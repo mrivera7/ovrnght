98dcb899b6670285d35d14387f292896bdf563d1
Michael A. Rivera <mrivera7@protonmail.com>
2019-10-17 12:21:43 -0700

    Serves static files

--- src/IndexController.ts
+++ src/IndexController.ts
@@ -1,25 +1,25 @@
 import * as path from 'path';
-import * as dotenv from 'dotenv';
 import { Request, Response, NextFunction } from 'express';
 import { Controller, Middleware, Get, Post, Put, Delete } from '@overnightjs/core';
 
-dotenv.config();
+const calcRoot: string = path.resolve(__dirname, "../../calculator/build");
 
 @Controller('/')
 export class IndexController {
     
-    @Get()
+    @Get('')
     get(req: Request, res: Response): any {
         // console.log(req.params.id);
         // return res.status(200).json({ msg: 'get_called' });
         
-        const rootDir: string = (process.env.ROOT_DIR as string);
-        // console.log(rootDir);
-        // console.log(__dirname);
-        return res.sendFile(path.resolve(rootDir, "calculator/build/index.html"));
-
-        // return res.sendFile(/** What do I put in here? */);
-
+        return res.sendFile(path.join(calcRoot, "index.html"));
     }
 
+    /**
+    @Get('static/css/*')
+    private Get(req: Request, res: Response): any {
+        console.log(req.baseUrl);
+        return res.sendFile(path.join(calcRoot, req.baseUrl));
+    }
+    */
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import * as path from 'path';
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
-
 import { Server } from '@overnightjs/core';
 import { IndexController } from './IndexController';
+
+const calcRoot: string = path.resolve(__dirname, "../../calculator/build");
 
 export class SampleServer extends Server {
     constructor() {
@@ -9,6 +12,7 @@ export class SampleServer extends Server {
         
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(express.static(calcRoot));
 
         let indexController = new IndexController();
 
